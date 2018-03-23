21e23589bf4f9ce3206a30c633bf8b18f2bde3f2
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-03-23 20:41:52 +0300

    fix: replace duplicate names

--- hasheroku/hasheroku.py
+++ hasheroku/hasheroku.py
@@ -16,11 +16,11 @@ ADJECTIVES = [
 NOUNS = [
     "waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
     "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter",
-    "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook",
+    "forest", "hill", "cloud", "meadow", "storm", "glade", "bird", "brook",
     "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly",
     "feather", "grass", "haze", "mountain", "night", "pond", "darkness",
     "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder",
-    "violet", "water", "wildflower", "wave", "water", "resonance", "sun",
+    "violet", "fiesta", "wildflower", "wave", "water", "resonance", "sun",
     "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper",
     "frog", "smoke", "star"
 ]
