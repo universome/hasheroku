40bc59471485b4c91f07793cead6a52126735b36
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-03-23 20:48:15 +0300

    chore: add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name='hasheroku',
+    version='0.1.0',
+    author='Ivan Skorokhodov',
+    author_email='iskorokhodov@gmail.com',
+    url='https://github.com/universome/hasheroku',
+    description='Generates nice heroku-ish hash from the string',
+    packages=find_packages(exclude=('test',)),
+    zip_safe=True
+)
