a83a03e672139d790bc7acf92178710eeee064a4
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-03-23 23:49:05 +0300

    chore: add LICENSE and README to setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='hasheroku',
-    version='0.1.1',
+    version='0.1.2',
     author='Ivan Skorokhodov',
     author_email='iskorokhodov@gmail.com',
     url='https://github.com/universome/hasheroku',
     description='Generates nice heroku-ish hash from the string',
     packages=find_packages(exclude=('test',)),
+    license='BSD',
+    long_description=open('README.rst').read(),
     zip_safe=True
 )
